4d9fc498babe39fee500e9054220dd6e29fb6c8b
Wiktor Jakubowski <w.jakubowski04@gmail.com>
2026-05-21 09:38:33 +0200

    integrated SQLite database for attack logs

--- honeypot_poc.py
+++ honeypot_poc.py
@@ -1,5 +1,24 @@
 import socket
 import datetime
+import sqlite3
+
+
+# connect to SQLite / creates the file if it doesn't exist
+db = sqlite3.connect("threat_logs.db")
+cursor = db.cursor()
+
+# table to store attack data / if it doesn't exist
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS attacks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        timestamp TEXT,
+        ip_address TEXT,
+         port INTEGER,
+         payload TEXT
+    )
+''')
+db.commit()
+
 
 # configuration of the trap
 HOST = '0.0.0.0' # listen on all available local network interfaces
@@ -13,7 +32,7 @@ print("-" * 50)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen() 
-    
+
     while True:
         # wait on silent for a connection
         conn, addr = s.accept()
@@ -34,6 +53,20 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             try:
                 data = conn.recv(1024)
                 if data:
-                    print(f"[*] Captured payload: {data.decode('utf-8', errors='ignore').strip()}")
-            except Exception:
+                    # decode the raw bytes into a readable string
+                    payload = data.decode('utf-8', errors='ignore').strip()
+                    print(f"[*] Captured payload: {payload}")
+                    
+                    # database Injection
+                    # save the captured details into the SQLite database
+                    cursor.execute('''
+                        INSERT INTO attacks (timestamp, ip_address, port, payload) 
+                        VALUES (?, ?, ?, ?)
+                    ''', (timestamp, attacker_ip, attacker_port, payload))
+                    
+                    db.commit() # save changes to the disk physically
+                    print("[*] Attack successfully logged to the database.")
+                    
+            except Exception as e:
+                # catch and ignore errors so the honeypot doesn't crash on bad inputs
                 pass
